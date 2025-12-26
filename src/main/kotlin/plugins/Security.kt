770e7e322913ff609f93f50a76026a8c0414c212
firkowski <firkowskikuba@gmail.com>
2025-12-26 23:06:34 +0200

    fixed problem with verifier

--- src/main/kotlin/plugins/Security.kt
+++ src/main/kotlin/plugins/Security.kt
@@ -3,28 +3,26 @@ package com.example.plugins
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.example.core.config.loadConfigJWT
+import com.example.plugins.Security.JwtService
+import com.example.plugins.Security.JwtServiceImpl
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 
 
 
-fun Application.configureSecurity() {
+fun Application.configureSecurity(jwtService: JwtServiceImpl) {
 
     val jwtConfig = loadConfigJWT()
 
     authentication {
         jwt("auth-jwt") {
             realm = jwtConfig.jwtRealm
-            verifier(
-                JWT
-                    .require(Algorithm.HMAC256(jwtConfig.jwtSecret))
-                    .withAudience(jwtConfig.jwtAudience)
-                    .withIssuer(jwtConfig.jwtIssuer)
-                    .build()
-            )
+
+            verifier(jwtService.verifier())
+
             validate { credential ->
-                if (credential.payload.audience.contains(jwtConfig.jwtAudience)) JWTPrincipal(credential.payload) else null
+                if(credential.)
             }
         }
     }
--- src/main/kotlin/plugins/Security/JwtServiceImpl.kt
+++ src/main/kotlin/plugins/Security/JwtServiceImpl.kt
@@ -1,16 +1,35 @@
 package com.example.plugins.Security
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
 import com.example.core.config.JwtConfig
+import java.util.Date
 
 class JwtServiceImpl(
     private val cfg: JwtConfig
 ): JwtService {
 
+    val algorithm = Algorithm.HMAC256(cfg.jwtSecret)
+
     override fun generateToken(userId: String, tenantId: String, role: String): String{
-        TODO("Not yes implemented")
+        return JWT.create()
+            .withIssuer(cfg.jwtIssuer)
+            .withAudience(cfg.jwtAudience)
+            .withSubject(userId)
+            .withClaim("tenant_id", tenantId)
+            .withClaim("role", role)
+            .withIssuedAt(Date(System.currentTimeMillis()))
+            .withExpiresAt(Date(System.currentTimeMillis() + 15 * 60 * 1000))                 // 15 min
+            .sign(algorithm)
     }
 
     override fun generateRefreshToken(userId: String, tenantId: String): String {
         TODO("Not yet implemented")
     }
+
+    fun verifier(): JWTVerifier = JWT.require(algorithm)
+        .withIssuer(cfg.jwtIssuer)
+        .withAudience(cfg.jwtAudience)
+        .build()
 }
